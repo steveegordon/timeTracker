1ffd55446705b0b13bda5f7956c74f6448ea97b3
Steven Gordon <tdtsgordon@gmail.com>
2024-01-31 11:10:02 -0800

    implement pool environmental variables

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE,
 });
 
 module.exports = pool;
