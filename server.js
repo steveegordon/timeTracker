5e1d295a72e5028caca33ab89ceef8a9bd4deb33
Steven Gordon <tdtsgordon@gmail.com>
2024-02-03 13:48:10 -0800

    add search by type, begin work on post method

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const pool = require('./db');
+const pool = require("./db");
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  pool.query("SELECT * FROM event_data", (error, results) => {
+app.get("/events", (req, res) => {
+  let type = ``;
+  if (req.query.type) {
+    type = ` WHERE type = '${req.query.type}'`;
+    console.log(type);
+  }
+  pool.query(`SELECT * FROM event_data${type};`, (error, results) => {
     if (error) throw error;
+    console.log(`returned all events${type}`);
     res.status(200).json(results.rows);
   });
 });
-app.listen(port, () => {console.log(`app listening on port ${port}`);});
+
+app.post("/events", (req, res) => {
+  const { start_time, end_time, type, description } = req.body;
+  pool.query(
+    `INSERT`,
+    [start_time, end_time, type, description],
+    (error, results) => {}
+  );
+});
+
+app.get("/events");
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
