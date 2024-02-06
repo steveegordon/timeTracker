10b6aa6696443a634dbeac21a35f85812c94d10e
stevegordon <tdtsgordon@gmail.com>
2024-02-06 09:44:24 -0800

    add database connection detail log

--- server.js
+++ server.js
@@ -14,21 +14,20 @@ app.get("/events", (req, res) => {
   pool.query(`SELECT * FROM event_data${type};`, (error, results) => {
     if (error) throw error;
     console.log(`returned all events${type}`);
-    res.status(200).json(results.rows);
+    return res.status(200).json(results.rows);
   });
 });
 
 app.post("/events", (req, res) => {
   const { start_time, end_time, type, description } = req.body;
   pool.query(
-    `INSERT`,
-    [start_time, end_time, type, description],
-    (error, results) => {}
-  );
+    `INSERT (start_time, end_time, type, description INTO event_data VALUES (${start_time}, ${end_time}, ${type}, ${descripton})`,
+    (error, results) => {
+      if (error) throw error;
+      });
 });
-
-app.get("/events");
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
+  console.log(`connecting to database at ${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`)
 });
